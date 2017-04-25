662157f1f56adcf8f4e480115e5d973f34679201
Daniel Nix <daniel.nix@gmail.com>
2017-04-25 14:40:09 +0200

    taking into account different type of toot url (@user/id & users/user/updates/id)

--- Main.php
+++ Main.php
@@ -1,9 +1,51 @@
 <?php
-    namespace IdnoPlugins\MastodonEmbed {
-        class Main extends \Idno\Common\Plugin {
-            function registerPages() {                
-                \Idno\Core\site()->template()->extendTemplate('entity/content/embed','mastodonembed/embed');
-                \Idno\Core\site()->template()->extendTemplate('shell/head', 'mastodonembed/shell/head');
-            }
+
+namespace IdnoPlugins\MastodonEmbed {
+
+    /**
+     * 
+     * @param string $url
+     * @return string $content
+     */
+    class Main extends \Idno\Common\Plugin {
+
+        function registerPages() {
+            \Idno\Core\site()->template()->extendTemplate('entity/content/embed', 'mastodonembed/embed');
         }
+
+         function getApi($toot) {
+            if ($url = parse_url($toot)) {
+                $res = \Idno\Core\Webservice::get($toot);
+                $content = json_decode($res['content']);
+ 
+                if (!empty($content)) {
+                    return $content->html;
+                }
+            }
+            return false;
+        }
+
+        static function getApiFromDom($toot) {
+            // \Idno\Core\Idno::site()->logging()->log("DEBUG TOOT " . var_export($toot, true));
+
+            if ($res = \Idno\Core\Webservice::get($toot)) {
+                $html = ($res['content']);
+                $dom = @\DOMDocument::loadHTML($html);
+                
+                $attributes = array();
+                foreach ($dom->getElementsByTagName('link') as $link) {
+                   
+                    $href = $link->getAttribute('href');
+                    $type = $link->getAttribute('type');
+                    
+                    if ($type == 'application/json+oembed') {
+                        return self::getApi($href);
+                    }
+                }
+            }
+            return false;
+        }
+
     }
+
+}
--- templates/default/mastodonembed/embed.tpl.php
+++ templates/default/mastodonembed/embed.tpl.php
@@ -2,72 +2,30 @@
 
 $embedded = '';
 
-$testembed = '<div class=\'activity-stream activity-stream-headless h-entry\'>
-<div class=\'entry entry-center\'>
-<div class=\'detailed-status light\'>
-<a class="detailed-status__display-name p-author h-card" rel="noopener" href="https://mastodon.social/@nxd4n"><div>
-<div class=\'avatar\'>
-<img width="48" height="48" alt="" class="u-photo" src="https://files.mastodon.social/accounts/avatars/000/027/955/original/64ff61f550bba6ad.jpeg?1491391555" />
-</div>
-</div>
-<span class=\'display-name\'>
-<strong class=\'p-name emojify\'>nxd4n</strong>
-<span>@nxd4n</span>
-</span>
-</a><div class=\'status__content p-name emojify\'><div class=\'e-content\' style=\'display: block; direction: ltr\'><p>GitHub - danito/KnownEmbedMastodon: Convert mastodon status URLs into quoted toots: <a href="https://github.com/danito/KnownEmbedMastodon" rel="nofollow noopener" target="_blank"><span class="invisible">https://</span><span class="ellipsis">github.com/danito/KnownEmbedMa</span><span class="invisible">stodon</span></a></p></div></div>
-<div class=\'detailed-status__meta\'>
-<data class=\'dt-published\' value=\'2017-04-20T12:28:57+00:00\'></data>
-<a class="detailed-status__datetime u-url u-uid" rel="noopener" href="https://mastodon.social/@nxd4n/3391735"><span>Apr 20, 2017, 12:28</span>
-</a>·
-<a class="detailed-status__application" target="_blank" rel="noopener" href="https://nxd4n.nixekinder.be/">nxd4n&#39;s Known</a>
-·
-<span><i class="fa fa-retweet"></i><span>0</span></span>
-·
-<span><i class="fa fa-star"></i><span>0</span></span>
-·
-<a class="open-in-web-link" target="_blank" href="https://mastodon.social/web/statuses/3391735">Open in web</a>
-</div>
-</div>
-
-</div>
-
-
-</div>';
-
 $body = Idno\Core\site()->triggerEvent('url/expandintext', ['object' => $vars['object']], $vars['object']->body);
+
+/**
+ * https://INST.ANCE/users/*USER/updates/$ID
+ */
 if (preg_match_all('/https?:\/\/[^\s]+\/users\/[^\s]+\/updates\/[^\s]+/i', $body, $matches)) {
     foreach ($matches[0] as $m) {
-        $url = parse_url($m);
-        $serverAPI = $url['scheme'].'://'.$url['host'].'/api/oembed.json?url='.$param = urlencode($m);
-        
-        $res = \Idno\Core\Webservice::get($serverAPI);
-        $content = json_decode($res['content']);
-        \Idno\Core\Idno::site()->logging()->log("GET CONTENT: " . var_export($content, true));
-              
-        if (!empty($content)) {
-            $html = $content->html;
+
+        if ($html = \IdnoPlugins\MastodonEmbed\Main::getApiFromDom($m)) {
             $embedded .= $html;
         }
-        //$embedded .= '<iframe src="' . $m . '/embed" style="overflow: hidden" frameborder="0" height="300"  width="600" scrolling="yes" onload="this.height=this.contentWindow.document.body.scrollHeight;"></iframe>';
-        //$embedded .= $embedded .= '<script src="' . $m . '.js"></script>';;
     }
 }
 //https://mastodon.social/@nxd4n/3391735
 if (preg_match_all('/https?:\/\/[^\s]+\/@[^\s]+\/[^\s]+/i', $body, $matches)) {
     foreach ($matches[0] as $m) {
-       $url = parse_url($m);
-        $serverAPI = $url['scheme'].'://'.$url['host'].'/api/oembed.json?url='.$param = urlencode($m);
         
-        $res = \Idno\Core\Webservice::get($serverAPI);
-        $content = json_decode($res['content']);
-        \Idno\Core\Idno::site()->logging()->log("GET CONTENT: " . var_export($content, true));
-              
-        if (!empty($content)) {
-            $html = $content->html;
+        if ($html = \IdnoPlugins\MastodonEmbed\Main::getApiFromDom($m)) {
             $embedded .= $html;
         }
-        //$embedded .= $embedded .= '<script src="' . $m . '.js"></script>';;
+       
     }
 }
+
+
 
 echo $embedded;
